b7889edf83bcd6c55df82551e27750cbbd6b66e3
Tommaullin <Tommaullin@gmail.com>
2019-11-14 13:16:26 +0000

    wip installing nilearn

--- sim/simRF.sh
+++ sim/simRF.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+$FSLDIR/fslpython/bin/conda create python=3.6 -p ./myenv
+source $FSLDIR/fslpython/bin/activate ./myenv
+pip install nilearn
 fslpython -c "from lib import simRF; simRF.main()"
