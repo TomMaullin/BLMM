e91171bb46a89f1c4e50509ac35ed4655ee5295c
TomMaullin <tommaullin@gmail.com>
2019-11-22 14:01:18 +0000

    cluster working

--- sim/simRF.sh
+++ sim/simRF.sh
@@ -1,6 +1,4 @@
 #!/bin/bash                                                                                                                                                                                    
-#$ -o $HOME/logpFS                                                                                                                                                                               
-#$ -e $HOME/logpFS
 
 source $FSLDIR/fslpython/bin/activate ./blmmenv
 python -c "from lib import simRF_DAC; simRF_DAC.main()"
