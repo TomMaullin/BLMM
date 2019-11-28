5f9df78c150bf61cdd384d67330b2f204ff47bcb
TomMaullin <tommaullin@gmail.com>
2019-11-28 13:52:21 +0000

    2nd design

--- simRF_cluster.sh
+++ simRF_cluster.sh
@@ -11,6 +11,6 @@ n=100
 while [ "$i" -le "$n" ]; do
 
   # Submit n simulations 
-  fsl_sub -l logpFS/ -N sim$i bash $BLM_PATH/sim/simRF.sh
+  fsl_sub -l logFS/ -N sim$i bash $BLM_PATH/sim/simRF.sh
   i=$(($i + 1))
 done
