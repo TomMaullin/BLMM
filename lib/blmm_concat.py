2828b229355e3f3399aae824f37a8e598db73920
TomMaullin <tommaullin@gmail.com>
2019-12-13 11:08:50 +0000

    wip adding nlevels, nparams

--- lib/blmm_concat.py
+++ lib/blmm_concat.py
@@ -81,8 +81,8 @@ def main(*args):
 
     for k in range(r):
 
-        rfxdes = rfxmats[k]['f' + str(i)]['design']
-        rfxfac = rfxmats[k]['f' + str(i)]['factor']
+        rfxdes = rfxmats[k]['f' + str(k+1)]['design']
+        rfxfac = rfxmats[k]['f' + str(k+1)]['factor']
 
         nparams = nparams + [rfxdes.shape[1]]
         nlevels = nlevels + [len(np.unique(rfxfac))]
