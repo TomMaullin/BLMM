2e7b238e145fda8e18bf813c9764ea23127e7f1e
Tommaullin <Tommaullin@gmail.com>
2019-12-14 20:23:19 +0000

    wip llh

--- lib/tools3d.py
+++ lib/tools3d.py
@@ -467,6 +467,10 @@ def llh3D(n, ZtZ, Zte, ete, sigma2, DinvIplusZtZD,D):
     if np.prod(n.shape) > 1:
 
       n = n.reshape(sigma2.shape)
+
+  print('llh shapes')
+  print(np.log(np.linalg.det(np.eye(ZtZ.shape[1]) + ZtZ @ D)).shape)
+  print(n*np.log(sigma2).shape)
   
   # Work out -1/2(nln(sigma^2) + ln|I+Z'ZD|)
   firstterm = -0.5*(n*np.log(sigma2) + np.log(np.linalg.det(np.eye(ZtZ.shape[1]) + ZtZ @ D))).reshape(ete.shape[0])
