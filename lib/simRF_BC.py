7deb02d6fbfd6062dbfdb89790d9324177223621
Tommaullin <Tommaullin@gmail.com>
2020-02-10 17:28:15 +0000

    wip strange cholmod behaviour

--- lib/simRF_BC.py
+++ lib/simRF_BC.py
@@ -307,7 +307,7 @@ def main():
 			try:
 				Lamij = np.linalg.cholesky(Dij)
 			except:
-				L, D, ~ = la.ldl(Dij)
+				L, D, perm = la.ldl(Dij)
 				Lamij = np.matmul(L, np.sqrt(D))
 				print(Lamij)
 
