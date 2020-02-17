1b6df2f8d3d0efa8dcca6460a1fdb047228dac2d
TomMaullin <tommaullin@gmail.com>
2020-02-17 12:56:55 +0000

    wip

--- lib/simRF_BC.py
+++ lib/simRF_BC.py
@@ -316,16 +316,20 @@ def main():
 			# Add it to running element list
 			vecuLami = np.concatenate((vecuLami, vecuLamij), axis=None)
 
+			if np.random.uniform(0,1,1)<0.01:
+				print('vec-check')
+				print(vecuLamij)
+				print(Lamij)
+
 		# Look at individual sigma2
 		sigma2i = sigma2[i]
 
+		if np.random.uniform(0,1,1)<0.01:
+			print('vecLami')
+			print(vecuLami)
+
 		# Compose theta vector
 		thetai = vecuLami*np.sqrt(sigma2i)
-
-		if np.random.uniform(0,1,1)<0.01:
-			print('vec-check')
-			print(vecuLami)
-			print(Lami)
 
 		# Add theta vector to array
 		theta[i,:] = thetai.reshape(theta[i,:].shape)
