d35d442619f4baa224d20af63b990e68b6ae9d49
TomMaullin <tommaullin@gmail.com>
2020-03-17 18:58:01 +0000

    began moving unit test code over for 2D

---
+++ test/Unit/unitTests2D.py
@@ -0,0 +1,9 @@
+# This file contains all unit tests for the functions given in the tools2D.py
+# file.
+
+
+import sys
+# We need to also import tools2D itself, so we first must add lib to the python path.
+sys.path.insert(1, 'sys.argv[0]/../../../lib/')
+
+import tools2d.py
