4dbb6c732b03699bd9300a9443209b97d01f1c56
Tommaullin <Tommaullin@gmail.com>
2020-04-05 21:24:36 +0100

    blmm_concat cleaned

--- src/blmm_concat.py
+++ src/blmm_concat.py
@@ -17,8 +17,38 @@ from lib.fileio import *
 import src.blmm_inference as blmm_inference
 import src.blmm_estimate as blmm_estimate
 
+# ------------------------------------------------------------------------------------
+#
+# This file is the third stage of the BLMM pipeline. This stage reads in the product
+# matrices output by each of the `blmm_batch` jobs during the second stage and s 
+# them to obtain the product matrices for the overall model. It also calculates n_sv
+# for the whole model and the overall mask.
+#
+# Following this, the `blmm_concat` code then seperates the voxels in the brain into
+# two categories; "inner" and "ring" (explained in the developer notes below). Once
+# this has been done the product matrices corresponding to "inner" and "ring" voxels
+# are passed to `blmm_estimate`, which estimates the parameters of the model; beta,
+# sigma2 and D. Following this, the product matrices and parameter estimates are 
+# passed to `blmm_inference`, which generates statistic maps and other miscelanoues 
+# output.
+#
+# ------------------------------------------------------------------------------------
+#
+# Author: Tom Maullin (Last edited: 04/04/2020)
+#
+# ------------------------------------------------------------------------------------
+#
+# The code takes the following inputs:
+#
+#  - input path (optional): If specified, the first argument will be ased to be a
+#                           path to an `inputs` yml file, following the same 
+#                           formatting guidelines as `blmm_config.yml`. If not 
+#                           specified, the default file `blmm_config.yml` will be 
+#                           ased to contain the inputs.
+#
+# ------------------------------------------------------------------------------------
 # Developer notes:
-# --------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------
 # In the following code I have used the following subscripts to indicate:
 #
 # _r - This means this is an array of values corresponding to voxels which
@@ -35,14 +65,12 @@ import src.blmm_estimate as blmm_estimate
 # _sv - This means this variable is spatially varying (There is a reading
 #       per voxel). 
 #
-# --------------------------------------------------------------------------
-# Author: Tom Maullin (Last edited 04/04/2020)
-
+# ------------------------------------------------------------------------------------
 def main(*args):
 
-    # ----------------------------------------------------------------------
+    # --------------------------------------------------------------------------------
     # Check inputs
-    # ----------------------------------------------------------------------
+    # --------------------------------------------------------------------------------
     if len(args)==0 or (not args[0]):
         # Load in inputs
         with open(os.path.join(
@@ -59,9 +87,9 @@ def main(*args):
             # In this case inputs structure is first argument.
             inputs = args[0]
 
-    # ----------------------------------------------------------------------
+    # --------------------------------------------------------------------------------
     # Read basic inputs
-    # ----------------------------------------------------------------------
+    # --------------------------------------------------------------------------------
     OutDir = inputs['outdir']
 
     # Random factor variables.
@@ -87,15 +115,15 @@ def main(*args):
     # Get number of rfx params
     nparams = np.array(nparams)
     nlevels = np.array(nlevels)
-    n_q = np.sum(nparams*nlevels)
+    q = np.(nparams*nlevels)
 
     # Get number of unique rfx params
-    n_q_u = np.sum(nparams*(nparams+1)//2)
+    q_u = np.(nparams*(nparams+1)//2)
     
     # Get number of parameters
     c1 = str2vec(inputs['contrasts'][0]['c' + str(1)]['vector'])
     c1 = np.array(c1)
-    n_p = c1.shape[0]
+    p = c1.shape[0]
     del c1
     
     # Read in the nifti size and work out number of voxels.
@@ -104,15 +132,14 @@ def main(*args):
         nifti = loadFile(nifti_path)
 
     NIFTIsize = nifti.shape
-    n_v = int(np.prod(NIFTIsize))
+    v = int(np.prod(NIFTIsize))
 
-    # ----------------------------------------------------------------------
+    # --------------------------------------------------------------------------------
     # Remove any files from the previous runs
     #
-    # Note: This is important as if we are outputting blocks to files we
-    # want to be sure none of the previous results are lingering around 
-    # anywhere.
-    # ----------------------------------------------------------------------
+    # Note: This is important as if we are outputting blocks to files we want to be
+    # sure none of the previous results are lingering around anywhere.
+    # --------------------------------------------------------------------------------
 
     files = ['blmm_vox_n.nii', 'blmm_vox_mask.nii', 'blmm_vox_edf.nii', 'blmm_vox_beta.nii',
              'blmm_vox_llh.nii', 'blmm_vox_sigma2.nii', 'blmm_vox_D.nii', 'blmm_vox_resms.nii',
@@ -126,10 +153,10 @@ def main(*args):
 
             os.remove(os.path.join(OutDir, file))
 
-    # ----------------------------------------------------------------------
+    # --------------------------------------------------------------------------------
     # Get n (number of observations) and n_sv (spatially varying number of
     # observations)
-    # ----------------------------------------------------------------------
+    # --------------------------------------------------------------------------------
 
     # Work out number of batchs
     n_b = len(glob.glob(os.path.join(OutDir,"tmp","blmm_vox_n_batch*")))
@@ -162,18 +189,18 @@ def main(*args):
                            nifti.affine,
                            header=nifti.header)
     nib.save(nmap, os.path.join(OutDir,'blmm_vox_n.nii'))
-    n_sv = n_sv.reshape(n_v, 1)
+    n_sv = n_sv.reshape(v, 1)
     del nmap
 
     # Get ns.
     X = loadFile(inputs['X'])
     n = X.shape[0]
 
-    # ----------------------------------------------------------------------
+    # --------------------------------------------------------------------------------
     # Create Mask
-    # ----------------------------------------------------------------------
+    # --------------------------------------------------------------------------------
 
-    Mask = np.ones([n_v, 1])
+    Mask = np.ones([v, 1])
 
     # Check for user specified missingness thresholds.
     if 'Missingness' in inputs:
@@ -220,14 +247,14 @@ def main(*args):
     # We remove anything with 1 degree of freedom (or less) by default.
     # 1 degree of freedom seems to cause broadcasting errors on a very
     # small percentage of voxels.
-    Mask[n_sv<=n_p+1]=0
+    Mask[n_sv<=p+1]=0
 
     if 'analysis_mask' in inputs:
 
         addmask_path = inputs["analysis_mask"]
         
         # Read in the mask nifti.
-        addmask = loadFile(addmask_path).get_data().reshape([n_v,1])
+        addmask = loadFile(addmask_path).get_data().reshape([v,1])
         
         Mask[addmask==0]=0
 
@@ -253,21 +280,21 @@ def main(*args):
     del Mask
 
     # Number of voxels in ring
-    n_v_r = R_inds.shape[0]
+    v_r = R_inds.shape[0]
 
     # Number of voxels in inner mask
-    n_v_i = I_inds.shape[0]
+    v_i = I_inds.shape[0]
 
     # Number of voxels in whole (inner + ring) mask
-    n_v_m = n_v_i + n_v_r
+    v_m = v_i + v_r
 
     # Create df map
-    df_r = n_sv[R_inds,:] - n_p
-    df_r = df_r.reshape([n_v_r])
-    df_i = n - n_p
+    df_r = n_sv[R_inds,:] - p
+    df_r = df_r.reshape([v_r])
+    df_i = n - p
 
     # Unmask df
-    df = np.zeros([n_v])
+    df = np.zeros([v])
     df[R_inds] = df_r
     df[I_inds] = df_i
 
@@ -282,18 +309,18 @@ def main(*args):
     nib.save(dfmap, os.path.join(OutDir,'blmm_vox_edf.nii'))
     del df, dfmap
 
-    # ----------------------------------------------------------------------
+    # --------------------------------------------------------------------------------
     # Load X'X, X'Y, Y'Y, X'Z, Y'Z, Z'Z
-    # ----------------------------------------------------------------------
+    # --------------------------------------------------------------------------------
     # Read the matrices from the first batch. Note XtY is transposed as np
     # handles lots of rows much faster than lots of columns.
-    sumXtY = np.load(os.path.join(OutDir,"tmp","XtY1.npy")).transpose()
-    sumYtY = np.load(os.path.join(OutDir,"tmp","YtY1.npy"))
-    sumZtY = np.load(os.path.join(OutDir,"tmp","ZtY1.npy"))
+    XtY = np.load(os.path.join(OutDir,"tmp","XtY1.npy")).transpose()
+    YtY = np.load(os.path.join(OutDir,"tmp","YtY1.npy"))
+    ZtY = np.load(os.path.join(OutDir,"tmp","ZtY1.npy"))
 
     # Work out the uniqueness mask for the spatially varying designs
     uniquenessMask = loadFile(os.path.join(OutDir,"tmp", 
-        "blmm_vox_uniqueM_batch1.nii")).get_data().reshape(n_v)
+        "blmm_vox_uniqueM_batch1.nii")).get_data().reshape(v)
 
     # Work out the uniqueness mask inside the ring around the brain
     uniquenessMask_r = uniquenessMask[R_inds]
@@ -312,9 +339,9 @@ def main(*args):
         os.path.join(OutDir,"tmp","XtX1.npy"))
 
     # Make zeros for outer ring of brain ZtZ, XtX, ZtX etc
-    ZtZ_batch_r = np.zeros((n_v_r, ZtZ_batch_unique.shape[1]))
-    ZtX_batch_r = np.zeros((n_v_r, ZtX_batch_unique.shape[1]))
-    XtX_batch_r = np.zeros((n_v_r, XtX_batch_unique.shape[1]))
+    ZtZ_batch_r = np.zeros((v_r, ZtZ_batch_unique.shape[1]))
+    ZtX_batch_r = np.zeros((v_r, ZtX_batch_unique.shape[1]))
+    XtX_batch_r = np.zeros((v_r, XtX_batch_unique.shape[1]))
 
     # Fill with unique maskings
     for m in range(1,maxM+1):
@@ -331,16 +358,14 @@ def main(*args):
             XtX_batch_i = XtX_batch_unique[(m-1),:]
 
     # Perform summation for ring
-    sumXtX_r = XtX_batch_r
-    sumZtX_r = ZtX_batch_r
-    sumZtZ_r = ZtZ_batch_r
+    XtX_r = XtX_batch_r
+    ZtX_r = ZtX_batch_r
+    ZtZ_r = ZtZ_batch_r
 
     # Perform summation for ring
-    sumXtX_i = XtX_batch_i
-    sumZtX_i = ZtX_batch_i
-    sumZtZ_i = ZtZ_batch_i
-
-    a = XtX_batch_i[0]
+    XtX_i = XtX_batch_i
+    ZtX_i = ZtX_batch_i
+    ZtZ_i = ZtZ_batch_i
 
     # Delete the files as they are no longer needed.
     os.remove(os.path.join(OutDir,"tmp","XtY1.npy"))
@@ -353,18 +378,18 @@ def main(*args):
     # Cycle through batches and add together results.
     for batchNo in range(2,(n_b+1)):
 
-        sumXtY = sumXtY + np.load(
+        XtY = XtY + np.load(
             os.path.join(OutDir,"tmp","XtY" + str(batchNo) + ".npy")).transpose()
 
-        sumYtY = sumYtY + np.load(
+        YtY = YtY + np.load(
             os.path.join(OutDir,"tmp","YtY" + str(batchNo) + ".npy"))
 
-        sumZtY = sumZtY + np.load(
+        ZtY = ZtY + np.load(
             os.path.join(OutDir,"tmp","ZtY" + str(batchNo) + ".npy"))
         
         # Read in uniqueness Mask file
         uniquenessMask = loadFile(os.path.join(OutDir,"tmp", 
-            "blmm_vox_uniqueM_batch" + str(batchNo) + ".nii")).get_data().reshape(n_v)
+            "blmm_vox_uniqueM_batch" + str(batchNo) + ".nii")).get_data().reshape(v)
 
         # Work out the uniqueness mask inside the ring around the brain
         uniquenessMask_r = uniquenessMask[R_inds]
@@ -384,9 +409,9 @@ def main(*args):
             os.path.join(OutDir,"tmp","XtX" + str(batchNo) + ".npy"))
 
         # Make zeros for whole nifti ZtZ, XtX, ZtX etc
-        ZtZ_batch_r = np.zeros((n_v_r, ZtZ_batch_unique.shape[1]))
-        ZtX_batch_r = np.zeros((n_v_r, ZtX_batch_unique.shape[1]))
-        XtX_batch_r = np.zeros((n_v_r, XtX_batch_unique.shape[1]))
+        ZtZ_batch_r = np.zeros((v_r, ZtZ_batch_unique.shape[1]))
+        ZtX_batch_r = np.zeros((v_r, ZtX_batch_unique.shape[1]))
+        XtX_batch_r = np.zeros((v_r, XtX_batch_unique.shape[1]))
 
         # Fill with unique maskings
         for m in range(1,maxM+1):
@@ -403,15 +428,13 @@ def main(*args):
                 XtX_batch_i = XtX_batch_unique[(m-1),:]
 
         # Add to running total
-        sumXtX_r = sumXtX_r + XtX_batch_r
-        sumZtX_r = sumZtX_r + ZtX_batch_r
-        sumZtZ_r = sumZtZ_r + ZtZ_batch_r
+        XtX_r = XtX_r + XtX_batch_r
+        ZtX_r = ZtX_r + ZtX_batch_r
+        ZtZ_r = ZtZ_r + ZtZ_batch_r
 
-        sumXtX_i = sumXtX_i + XtX_batch_i
-        sumZtX_i = sumZtX_i + ZtX_batch_i
-        sumZtZ_i = sumZtZ_i + ZtZ_batch_i
-
-        a = a + XtX_batch_i[0]
+        XtX_i = XtX_i + XtX_batch_i
+        ZtX_i = ZtX_i + ZtX_batch_i
+        ZtZ_i = ZtZ_i + ZtZ_batch_i
         
         # Delete the files as they are no longer needed.
         os.remove(os.path.join(OutDir, "tmp","XtY" + str(batchNo) + ".npy"))
@@ -422,44 +445,44 @@ def main(*args):
         os.remove(os.path.join(OutDir, "tmp","ZtZ" + str(batchNo) + ".npy"))
         os.remove(os.path.join(OutDir, "tmp", "blmm_vox_uniqueM_batch" + str(batchNo) + ".nii"))
 
-    # ----------------------------------------------------------------------
+    # --------------------------------------------------------------------------------
     # Calculate betahat = (X'X)^(-1)X'Y and output beta maps
-    # ----------------------------------------------------------------------    
+    # --------------------------------------------------------------------------------    
 
     # Reshaping
-    sumXtY = sumXtY.transpose()
+    XtY = XtY.transpose()
 
-    sumXtY = sumXtY.reshape([n_v, n_p, 1])
-    sumYtY = sumYtY.reshape([n_v, 1, 1])
-    sumZtY = sumZtY.reshape([n_v, n_q, 1])
+    XtY = XtY.reshape([v, p, 1])
+    YtY = YtY.reshape([v, 1, 1])
+    ZtY = ZtY.reshape([v, q, 1])
 
-    sumXtX_r = sumXtX_r.reshape([n_v_r, n_p, n_p])
-    sumZtX_r = sumZtX_r.reshape([n_v_r, n_q, n_p])
-    sumZtZ_r = sumZtZ_r.reshape([n_v_r, n_q, n_q])
+    XtX_r = XtX_r.reshape([v_r, p, p])
+    ZtX_r = ZtX_r.reshape([v_r, q, p])
+    ZtZ_r = ZtZ_r.reshape([v_r, q, q])
 
-    sumXtX_i = sumXtX_i.reshape([1, n_p, n_p])
-    sumZtX_i = sumZtX_i.reshape([1, n_q, n_p])
-    sumZtZ_i = sumZtZ_i.reshape([1, n_q, n_q])
+    XtX_i = XtX_i.reshape([1, p, p])
+    ZtX_i = ZtX_i.reshape([1, q, p])
+    ZtZ_i = ZtZ_i.reshape([1, q, q])
 
     REML = False
 
     # If we have indices where only some studies are present, work out X'X and
     # X'Y for these studies.
-    if n_v_r:
+    if v_r:
 
         # Calculate masked X'Y for ring
-        XtY_r = sumXtY[R_inds,:,:]
+        XtY_r = XtY[R_inds,:,:]
 
         # Calculate Y'Y for ring
-        YtY_r = sumYtY[R_inds,:,:]
+        YtY_r = YtY[R_inds,:,:]
 
         # Calculate masked Z'Y for ring
-        ZtY_r = sumZtY[R_inds,:,:]
+        ZtY_r = ZtY[R_inds,:,:]
 
         # We rename these for convinience
-        XtX_r = sumXtX_r
-        ZtZ_r = sumZtZ_r
-        ZtX_r = sumZtX_r
+        XtX_r = XtX_r
+        ZtZ_r = ZtZ_r
+        ZtX_r = ZtX_r
 
         # We calculate these by transposing
         YtX_r = XtY_r.transpose((0,2,1))
@@ -470,32 +493,32 @@ def main(*args):
         n_sv_r = n_sv[R_inds,:]
 
         # Clear some memory
-        del sumXtX_r, sumZtX_r, sumZtZ_r
+        del XtX_r, ZtX_r, ZtZ_r
 
 
     # If we have indices where all studies are present, work out X'X and
     # X'Y for these studies.
-    if n_v_i:
+    if v_i:
         
         # X'X must be 1 by np by np for broadcasting
-        XtX_i = sumXtX_i.reshape([1, n_p, n_p])
+        XtX_i = XtX_i.reshape([1, p, p])
 
-        XtY_i = sumXtY[I_inds,:]
-
-        # Calculate Y'Y for inner
-        YtY_i = sumXtY[I_inds,:]
+        XtY_i = XtY[I_inds,:]
 
         # Calculate Y'Y for inner
-        YtY_i = sumYtY[I_inds,:,:]
+        YtY_i = XtY[I_inds,:]
+
+        # Calculate Y'Y for inner
+        YtY_i = YtY[I_inds,:,:]
 
         # Calculate masked Z'X for inner
-        ZtX_i = sumZtX_i.reshape([1, n_q, n_p])
+        ZtX_i = ZtX_i.reshape([1, q, p])
 
         # Calculate masked Z'Y for inner
-        ZtY_i = sumZtY[I_inds,:,:]
+        ZtY_i = ZtY[I_inds,:,:]
 
         # Calculate Z'Y for inner
-        ZtZ_i = sumZtZ_i.reshape([1, n_q, n_q])
+        ZtZ_i = ZtZ_i.reshape([1, q, q])
 
         # We calculate these by transposing
         YtX_i = XtY_i.transpose((0,2,1))
@@ -503,12 +526,12 @@ def main(*args):
         XtZ_i = ZtX_i.transpose((0,2,1))
 
         # Clear some memory
-        del sumXtX_i, sumZtX_i, sumZtZ_i
-        del sumXtY, sumYtY, sumZtY
+        del XtX_i, ZtX_i, ZtZ_i
+        del XtY, YtY, ZtY
 
 
     # Complete parameter vector
-    if n_v_r:
+    if v_r:
 
         # Run parameter estimation
         beta_r, sigma2_r, D_r = blmm_estimate.main(inputs, R_inds, XtX_r, XtY_r, ZtX_r, ZtY_r, ZtZ_r, XtZ_r, YtZ_r, YtY_r, YtX_r, n_sv_r, nlevels, nparams)
@@ -516,7 +539,7 @@ def main(*args):
         # Run inference
         blmm_inference.main(inputs, nparams, nlevels, R_inds, beta_r, D_r, sigma2_r, n_sv_r, XtX_r, XtY_r, XtZ_r, YtX_r, YtY_r, YtZ_r, ZtX_r, ZtY_r, ZtZ_r)       
         
-    if n_v_i:
+    if v_i:
 
         # Run parameter estimation
         beta_i, sigma2_i, D_i = blmm_estimate.main(inputs, I_inds, XtX_i, XtY_i, ZtX_i, ZtY_i, ZtZ_i, XtZ_i, YtZ_i, YtY_i, YtX_i, n, nlevels, nparams)
