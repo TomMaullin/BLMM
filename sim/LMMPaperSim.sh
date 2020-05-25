772bfdfcc18c22b7ae7a219183184d1e1b452e96
Tommaullin <Tommaullin@gmail.com>
2020-05-25 22:18:28 +0100

    wip

--- sim/LMMPaperSim.sh
+++ sim/LMMPaperSim.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-fslpython -c "from sim import LMMPaperSim; LMMPaperSim.sim2D(desInd=3, OutDir='/well/nichols/users/inf852/PaperSims')"
+fslpython -c "from sim import LMMPaperSim; LMMPaperSim.timings(desInd=3, OutDir='/well/nichols/users/inf852/PaperSims')"
