e92fc6d05604f59a7fd933676f08a2e21a7c3a4a
Tommaullin <Tommaullin@gmail.com>
2020-05-25 14:34:53 +0100

    removed PeLS, doesnt work well on cluster

--- sim/LMMPaperSim.sh
+++ sim/LMMPaperSim.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-fslpython -c "from sim import LMMPaperSim; sim2D(OutDir='$1',desInd=int('$2'))"
+fslpython -c "from sim import LMMPaperSim; LMMPaperSim.sim2D(OutDir='$1',desInd=int('$2'))"
