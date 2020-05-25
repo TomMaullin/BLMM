0367defa6b32d5664e8c1280a5920d3532ed4faa
Tommaullin <Tommaullin@gmail.com>
2020-05-25 14:29:41 +0100

    wip making paper sims work

--- LMMPaperSim_cluster.sh
+++ LMMPaperSim_cluster.sh
@@ -1,2 +1,12 @@
 OutDir=$1
-fsl_sub -l log/ -N sim1 bash $BLMM_PATH/sim/LMMPaperSim.sh $OutDir 1
+
+# -----------------------------------------------------------------------
+# Work out LMM path
+# -----------------------------------------------------------------------
+RealPath() {
+    (echo $(cd $(dirname "$1") && pwd -P)/$(basename "$1"))
+}
+
+LMM_PATH=$(dirname $(RealPath "${BASH_SOURCE[0]}"))
+
+fsl_sub -l log/ -N sim1 bash $LMM_PATH/sim/LMMPaperSim.sh $OutDir 1
