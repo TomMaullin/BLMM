4ed8d88451a003a516031c1fde5207490b07de52
Tommaullin <Tommaullin@gmail.com>
2020-05-29 22:07:58 +0100

    wip T statistics

--- sim/LMMPaperSim.sh
+++ sim/LMMPaperSim.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#fslpython -c "from sim import LMMPaperSim; LMMPaperSim.sim2D(desInd=3, OutDir='/well/nichols/users/inf852/PaperSims')"
-fslpython -c "from sim import LMMPaperSim; LMMPaperSim.TstatisticPPplots(desInd=3, OutDir='/well/nichols/users/inf852/PaperSims')"
+fslpython -c "from sim import LMMPaperSim; LMMPaperSim.sim2D(desInd=1, OutDir='/well/nichols/users/inf852/PaperSims')"
+#fslpython -c "from sim import LMMPaperSim; LMMPaperSim.TstatisticPPplots(desInd=1, OutDir='/well/nichols/users/inf852/PaperSims')"
