dda15af4cb9b5299c099c900d8c0b9ce9e767c7e
Tommaullin <Tommaullin@gmail.com>
2020-06-16 00:47:14 +0100

    testing in initDk

--- lib/npMatrix3d.py
+++ lib/npMatrix3d.py
@@ -345,6 +345,9 @@ def initDk3D(k, ZtZ, Zte, sigma2, nlevels, nraneffs, dupMatTdict):
   # Number of voxels v
   v = Zte.shape[0]
 
+  # Number of random factors
+  r = len(nraneffs)
+
   # Small check on sigma2
   if len(sigma2.shape) > 1:
 
