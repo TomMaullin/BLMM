1f16b6736c2ee855c237a11a418b24b06ef838f1
Tommaullin <Tommaullin@gmail.com>
2020-12-21 22:58:09 +0000

    wip sh scripts

--- sim/blmm_sim_cluster.sh
+++ sim/blmm_sim_cluster.sh
@@ -9,17 +9,14 @@ RealPath() {
     (echo $(cd $(dirname "$1") && pwd -P)/$(basename "$1"))
 }
 
-BLMM_PATH=$(dirname $(RealPath "${BASH_SOURCE[0]}"))
+SIM_PATH=$(dirname $(RealPath "${BASH_SOURCE[0]}"))
 
-echo $BLMM_PATH
-
-# include parse_yaml function
-. $BLMM_PATH/scripts/parse_yaml.sh
+echo $SIM_PATH
 
 # -----------------------------------------------------------------------
 # Submit data generation job
 # -----------------------------------------------------------------------
-fsl_sub -l sim/sim$simInd/logDataGen/ -N dataGen$simInd bash $BLMM_PATH/sim/generate_data.sh $i $inputs > /tmp/$$ && dataGenID=$(awk 'match($0,/[0-9]+/){print substr($0, RSTART, RLENGTH)}' /tmp/$$)
+fsl_sub -l sim/sim$simInd/logDataGen/ -N dataGen$simInd bash $SIM_PATH/generate_data.sh $i $inputs > /tmp/$$ && dataGenID=$(awk 'match($0,/[0-9]+/){print substr($0, RSTART, RLENGTH)}' /tmp/$$)
 
 # This loop waits for the data generation job to finish before
 # deciding how many batches to run. It also checks to see if the data
@@ -31,14 +28,14 @@ do
 
   # obtain number of batches
   sleep 1
-  if [ -s $BLMM_PATH/sim/sim$simInd/data/nb.txt ]; then
-    typeset -i nb=$(cat $BLMM_PATH/sim/sim$simInd/data/nb.txt)
+  if [ -s $SIM_PATH/sim$simInd/data/nb.txt ]; then
+    typeset -i nb=$(cat $SIM_PATH/sim$simInd/data/nb.txt)
   fi
   i=$(($i + 1))
 
   # Check for error
   if [ $i -gt 30 ]; then
-    errorlog="/sim/sim$simInd/logDataGen/dataGen.e$setupID"
+    errorlog="/sim$simInd/logDataGen/dataGen.e$setupID"
     if [ -s $errorlog ]; then
       echo "Data generation has errored"
       exit
@@ -58,7 +55,7 @@ i=1
 while [ "$i" -le "$nb" ]; do
 
   # Submit nb batches and get the ids for them
-  fsl_sub -j $setupID -l log/ -N batch${i} bash $BLMM_PATH/scripts/cluster_blmm_batch.sh $i $inputs > /tmp/$$ && batchIDs=$(awk 'match($0,/[0-9]+/){print substr($0, RSTART, RLENGTH)}' /tmp/$$),$batchIDs
+  fsl_sub -j $setupID -l log/ -N batch${i} bash $SIM_PATH/scripts/cluster_blmm_batch.sh $i $inputs > /tmp/$$ && batchIDs=$(awk 'match($0,/[0-9]+/){print substr($0, RSTART, RLENGTH)}' /tmp/$$),$batchIDs
   i=$(($i + 1))
 done
 if [ "$batchIDs" == "" ] ; then
@@ -70,7 +67,7 @@ fi
 # -----------------------------------------------------------------------
 # Submit Concatenation job
 # -----------------------------------------------------------------------
-# fsl_sub -j $batchIDs -l log/ -N concat bash $BLMM_PATH/scripts/cluster_blmm_concat.sh $inputs > /tmp/$$ && concatID=$(awk 'match($0,/[0-9]+/){print substr($0, RSTART, RLENGTH)}' /tmp/$$)
+# fsl_sub -j $batchIDs -l log/ -N concat bash $SIM_PATH/scripts/cluster_blmm_concat.sh $inputs > /tmp/$$ && concatID=$(awk 'match($0,/[0-9]+/){print substr($0, RSTART, RLENGTH)}' /tmp/$$)
 # if [ "$concatID" == "" ] ; then
 #   echo "Concatenation job submission failed!"
 # else
