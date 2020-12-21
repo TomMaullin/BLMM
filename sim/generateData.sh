042de82b3885b9ba9744d0ed76120c9d9c959794
Tommaullin <Tommaullin@gmail.com>
2020-12-21 23:09:28 +0000

    data generation shell scripts

--- sim/generateData.sh
+++ sim/generateData.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-fslpython -c "from sim import generateData; generateData.generate_data(100, np.array([100,100,100]), $1, $2)"
+fslpython -c "from sim import generateData; generateData.generate_data(100, np.array([100,100,100]), '$1', $2)"
