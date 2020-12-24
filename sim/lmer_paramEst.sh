339a4a975b1e918a256bd044131e5ee154158b20
Tommaullin <Tommaullin@gmail.com>
2020-12-24 18:12:49 +0000

    getting R running

--- sim/lmer_paramEst.sh
+++ sim/lmer_paramEst.sh
@@ -4,4 +4,4 @@
 module load R/3.4.3 
 
 # Run script
-R CMD BATCH --no-save --no-restore '--args simInd=$1 batchNo=$2 outDir="$3"' $SIM_PATH/lmer_paramEst.R sim/sim$simInd/logDataGen/Rout.txt &
+R CMD BATCH --no-save --no-restore '--args simInd=$1 batchNo=$2 outDir="$3"' $SIM_PATH/lmer_paramEst.R $SIM_PATH/sim$simInd/logDataGen/Rout.txt &
