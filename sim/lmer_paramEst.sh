753e06758d60a2f89539271892188b58da2d86ea
Tommaullin <Tommaullin@gmail.com>
2020-12-24 19:17:19 +0000

    getting R running

--- sim/lmer_paramEst.sh
+++ sim/lmer_paramEst.sh
@@ -10,6 +10,6 @@ echo $3
 echo $3/sim$1/logDataGen/Rout.txt
 
 # Run script
-R CMD BATCH '--args simInd=$1 batchNo=$2 outDir="$3"' $3/lmer_paramEst.R $3/sim$1/logDataGen/Rout.txt &
+R CMD BATCH --no-save --no-restore '--args simInd=$1 batchNo=$2 outDir="$3"' $3/lmer_paramEst.R $3/sim$1/logDataGen/Rout.txt
 
 echo "here"
