c0801267fcdddd553602202dffebf67b8cd96a4c
Tommaullin <Tommaullin@gmail.com>
2020-12-30 19:14:12 +0000

    wip cleanup script

--- sim/cleanup.py
+++ sim/cleanup.py
@@ -68,6 +68,9 @@ def cleanup(OutDir,simNo):
     nlevels = np.array(nlevels)
     q = np.sum(nraneffs*nlevels)
 
+    # Number of covariance parameters
+    ncov = q*(q+1)//2
+
     # -----------------------------------------------------------------------
     # Get number of observations and fixed effects
     # -----------------------------------------------------------------------
@@ -75,7 +78,7 @@ def cleanup(OutDir,simNo):
     n = X.shape[0]
     p = X.shape[1]
 
-    print('n, p, q: ', n, p, q)
+    print('n, p, q, ncov: ', n, p, q, ncov)
 
     # -----------------------------------------------------------------------
     # Get number voxels and dimensions
@@ -85,7 +88,7 @@ def cleanup(OutDir,simNo):
     nmap = os.path.join(simDir, "BLMM", "blmm_vox_n.nii")
 
     # Work out dim if we don't already have it
-    dim = nib.Nifti1Image.from_filename(nmap, mmap=False).shape
+    dim = nib.Nifti1Image.from_filename(nmap, mmap=False).shape[:3]
 
     # Work out affine
     affine = nib.Nifti1Image.from_filename(nmap, mmap=False).affine.copy()
@@ -108,7 +111,7 @@ def cleanup(OutDir,simNo):
     # -----------------------------------------------------------------------
 
     # List all beta parameter files for simulation
-    beta_files = glob.glob(os.path.join(SimDir,'lmer', 'beta_*'))
+    beta_files = glob.glob(os.path.join(simDir,'lmer', 'beta_*'))
 
     # Work out number of groups we have to split indices into.
     nvg = len(beta_files)
@@ -118,7 +121,7 @@ def cleanup(OutDir,simNo):
     voxelGroups = np.array_split(np.arange(v), nvg)
 
     # Loop through each file reading in one at a time and adding to nifti
-    for cv, beta_file in enumerate(beta_files):
+    for cv in np.arange(nvg):
 
         # Current group of voxels
         inds_cv = voxelGroups[cv]
@@ -126,10 +129,12 @@ def cleanup(OutDir,simNo):
         # Number of voxels currently
         v_current = len(inds_cv)
 
-        print('beta file: ', beta_file)
+        # -------------------------------------------------------------------
+        # Beta combine
+        # -------------------------------------------------------------------
 
         # Read in file
-        beta_current = pd.io.parsers.read_csv(filepath, header=None).values
+        beta_current = pd.io.parsers.read_csv(os.path.join(simDir, 'lmer', 'beta_' + str(cv) + '.csv'), header=None).values
 
         print('beta_current shape', beta_current.shape)
 
@@ -137,11 +142,40 @@ def cleanup(OutDir,simNo):
         for param in np.arange(p):
 
             # Add back to a NIFTI file
-            addBlockToNifti(os.path.join(simDir,"lmer","lmer_beta.nii"), beta_current[:,param], inds_cv, volInd=param,dim=dim)
+            addBlockToNifti(os.path.join(simDir,"lmer","lmer_vox_beta.nii"), beta_current[:,param], inds_cv, volInd=param,dim=dim)
 
+        # -------------------------------------------------------------------
+        # Sigma2 combine
+        # -------------------------------------------------------------------
 
+        # Read in file
+        sigma2_current = pd.io.parsers.read_csv(os.path.join(simDir, 'lmer', 'sigma2_' + str(cv) + '.csv'), header=None).values
 
+        # Add back to a NIFTI file
+        addBlockToNifti(os.path.join(simDir,"lmer","lmer_vox_sigma2.nii"), sigma2_current, inds_cv, volInd=param,dim=dim)
 
+        # -------------------------------------------------------------------
+        # vechD combine
+        # -------------------------------------------------------------------
+
+        # Read in file
+        vechD_current = pd.io.parsers.read_csv(os.path.join(simDir, 'lmer', 'vechD' + str(cv) + '.csv'), header=None).values
+
+        # Loop through covariance parameters adding them one voxel at a time
+        for param in np.arange(ncov):
+
+            # Add back to a NIFTI file
+            addBlockToNifti(os.path.join(simDir,"lmer","lmer_vox_D.nii"), vechD_current[:,param], inds_cv, volInd=param,dim=dim)
+
+        # -------------------------------------------------------------------
+        # Log-likelihood combine
+        # -------------------------------------------------------------------
+
+        # Read in file
+        llh_current = pd.io.parsers.read_csv(os.path.join(simDir, 'lmer', 'llh_' + str(cv) + '.csv'), header=None).values
+
+        # Add back to a NIFTI file
+        addBlockToNifti(os.path.join(simDir,"lmer","lmer_vox_llh.nii"), llh_current, inds_cv, volInd=param,dim=dim)
 
 
     # write.csv(betas,paste(lmerDir,'/beta_',toString(batchNo),'.csv',sep=''), row.names = FALSE)
@@ -151,8 +185,16 @@ def cleanup(OutDir,simNo):
     # write.csv(tct,paste(lmerDir,'/time_',toString(batchNo),'.csv',sep=''), row.names = FALSE)
     # write.csv(nvox_est,paste(lmerDir,'/v_est_',toString(batchNo),'.csv',sep=''), row.names = FALSE)
 
-
-
+    # -----------------------------------------------------------------------
+    # Remove BLMM maps we are not interested in (for memory purposes)
+    # -----------------------------------------------------------------------
+    os.remove(os.path.join(simDir, 'BLMM', 'blmm_vox_con.nii'))
+    os.remove(os.path.join(simDir, 'BLMM', 'blmm_vox_conSE.nii'))
+    os.remove(os.path.join(simDir, 'BLMM', 'blmm_vox_conT.nii'))
+    os.remove(os.path.join(simDir, 'BLMM', 'blmm_vox_conT_swedf.nii'))
+    os.remove(os.path.join(simDir, 'BLMM', 'blmm_vox_edf.nii'))
+    os.remove(os.path.join(simDir, 'BLMM', 'blmm_vox_mask.nii'))
+    os.remove(os.path.join(simDir, 'BLMM', 'blmm_vox_n.nii'))
 
     # -----------------------------------------------------------------------
     # P value counts for histograms
