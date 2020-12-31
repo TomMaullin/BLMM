18f0cde9d2209c7a161e8e0a9d4728c862e6b32f
Tommaullin <Tommaullin@gmail.com>
2020-12-31 00:31:51 +0000

    upping sample size

--- sim/generateData.sh
+++ sim/generateData.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-fslpython -c "from sim import generateData; generateData.generate_data(100, [100,100,100], '$1', $2)"
+fslpython -c "from sim import generateData; generateData.generate_data(200, [100,100,100], '$1', $2)"
