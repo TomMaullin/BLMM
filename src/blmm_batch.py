e02a39093ce4db64438d7531c4223ef3807a32fa
Tommaullin <Tommaullin@gmail.com>
2021-04-13 03:19:41 +0100

    wip

--- src/blmm_batch.py
+++ src/blmm_batch.py
@@ -126,7 +126,7 @@ def main(*args):
 
     # Similar to blksize in SwE, we divide by 8 times the size of a nifti
     # to work out how many blocks we use.
-    blksize = int(np.floor(MAXMEM/8/NIFTImem/p));
+    blksize = np.floor(MAXMEM/NIFTImem/q);
 
     # Reduce X to X for this block.
     X = loadFile(inputs['X'])
