d500e739be4ed98ae19348da4e1f3ab817ed311e
Tommaullin <Tommaullin@gmail.com>
2021-07-01 23:52:18 +0100

    small fix

--- lmer/lmer_paramEst.sh
+++ lmer/lmer_paramEst.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Get the data in a nice format for R
-fslpython -c "from lmer import Rpreproc; Rpreproc.Rpreproc('$1', $2, $3)"
+fslpython -c "from lmer import Rpreproc; Rpreproc.Rpreproc('$2', $3, $4)"
 
 # Load R
 module load R/3.4.3 
 
 # Run parameter estimation
-R CMD BATCH --no-save --no-restore '--args batchNo='$3' outDir="'$1'" ' $1/lmer_paramEst.R $1/lmerlog/Rout'_'$3.txt
+R CMD BATCH --no-save --no-restore '--args batchNo='$4' outDir="'$2'" ' $1/lmer/lmer_paramEst.R $1/lmerlog/Rout'_'$4.txt
 
 # Cleanup the files created
-fslpython -c "from lmer import cleanup; cleanup.Rcleanup('$1', $2, $3)"
+fslpython -c "from lmer import cleanup; cleanup.Rcleanup('$2', $3, $4)"
