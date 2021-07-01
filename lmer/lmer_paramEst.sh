ad5a9b6905b3289618806e26bb33be7465e85092
Tommaullin <Tommaullin@gmail.com>
2021-07-02 00:46:13 +0100

    wip

--- lmer/lmer_paramEst.sh
+++ lmer/lmer_paramEst.sh
@@ -7,7 +7,7 @@ fslpython -c "from lmer import Rpreproc; Rpreproc.Rpreproc('$2', $3, $4)"
 module load R/3.4.3 
 
 # Run parameter estimation
-R CMD BATCH --no-save --no-restore '--args batchNo='$4' outDir="'$2'" ' $1/lmer/lmer_paramEst.R $1/lmerlog/Rout'_'$4.txt
+R CMD BATCH --no-save --no-restore '--args batchNo='$4' outDir="'$2'" ' $1/lmer/lmer_paramEst.R $2/lmerlog/Rout'_'$4.txt
 
 # Cleanup the files created
 fslpython -c "from lmer import cleanup; cleanup.Rcleanup('$2', $3, $4)"
