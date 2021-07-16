360c5c0012b4778ba8c62da72fc01a21c70946ad
Tommaullin <Tommaullin@gmail.com>
2021-07-16 18:58:32 +0100

    cleanup of sim and added safeMode

--- sim/cleanup.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-fslpython -c "from sim import cleanup; cleanup.cleanup('$1', $2)"
--- sim/generateData.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-fslpython -c "from sim import generateData; generateData.generate_data(500, [100,100,100], '$1', $2, $3)"
