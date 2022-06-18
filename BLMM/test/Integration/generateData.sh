0e6cd6ac7e99939d8e49b77b5e31321d930749fc
TomMaullin <tommaullin@gmail.com>
2022-06-18 14:47:40 +0100

    fixing import error, adding init, fixing ci

--- BLMM/test/Integration/generateData.sh
+++ BLMM/test/Integration/generateData.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 -c "from test.Integration import generateData; generateData.generate_data(500, [100,100,100], '$1', $2, $3)"
+python3 -c "from BLMM.test.Integration import generateData; generateData.generate_data(500, [100,100,100], '$1', $2, $3)"
